edab2be6fefea91df3cd64908a443f3e34d981fa
dannyenfp <daniel.valmas@gmail.com>
2016-10-27 14:34:08 -0400

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,3 +1,19 @@
 class Ride < ActiveRecord::Base
-  # write associations here
+    belongs_to :user
+    belongs_to :attraction
+
+def take_ride
+  if (self.user.height < self.attraction.min_height) && (self.user.tickets < self.attraction.tickets)
+      return  "Sorry. You do not have enough tickets the #{attraction.name}. You are not tall enough to ride the #{attraction.name}."
+    elsif self.user.height < self.attraction.min_height
+      return "Sorry. You are not tall enough to ride the #{attraction.name}."
+    elsif self.user.tickets < self.attraction.tickets
+     return "Sorry. You do not have enough tickets the #{attraction.name}."
+    else
+      user.update(tickets: (self.user.tickets - self.attraction.tickets), 
+                  nausea: (self.user.nausea + self.attraction.nausea_rating),
+                  happiness: (self.user.happiness + self.attraction.happiness_rating))
+      return "Thanks for riding the #{attraction.name}!"
+     end
+  end
 end
